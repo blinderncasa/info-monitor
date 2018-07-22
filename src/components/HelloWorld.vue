afa0b1875cb7e054b86d34d7074e2cdfaa97b22b
Henrik Lilleengen <henrik@cloudinsurance.io>
2018-07-22 18:57:02 +0200

    Add metro-depatures and clock

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,60 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>{{ `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}` }}</h2>
+    <div>
+      <p>Mot sentrum:</p>
+      <p>{{ getDeparturesFromPlatform(departures, "1").length > 0 ? getDeparturesFromPlatform(departures, "1")[0].lineCode + ' - ' + getDeparturesFromPlatform(departures, "1")[0].destination + ' (' + getHumanTime(getDeparturesFromPlatform(departures, "1")[0].depature) + ' min)': '' }}</p>
+      <p>{{ getDeparturesFromPlatform(departures, "1").length > 1 ? getDeparturesFromPlatform(departures, "1")[1].lineCode + ' - ' + getDeparturesFromPlatform(departures, "1")[1].destination + ' (' + getHumanTime(getDeparturesFromPlatform(departures, "1")[1].depature) + ' min)': '' }}</p>
+    </div>
+    <div>
+      <p>Mot ringen:</p>
+      <p>{{ getDeparturesFromPlatform(departures, "2").length > 0 ? getDeparturesFromPlatform(departures, "2")[0].lineCode + ' - ' + getDeparturesFromPlatform(departures, "2")[0].destination + ' (' + getHumanTime(getDeparturesFromPlatform(departures, "2")[0].depature) + ' min)': '' }}</p>
+      <p>{{ getDeparturesFromPlatform(departures, "2").length > 1 ? getDeparturesFromPlatform(departures, "2")[1].lineCode + ' - ' + getDeparturesFromPlatform(departures, "2")[1].destination + ' (' + getHumanTime(getDeparturesFromPlatform(departures, "2")[1].depature) + ' min)': '' }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      departures: [],
+      now: new Date()
+    }
+  },
+  mounted () {
+    this.loadData();
+
+    setInterval(function () {
+      this.loadData();
+    }.bind(this), 30000);
+
+    setInterval(() => {
+      this.now = new Date()
+    }, 1000);
+  },
+  methods: {
+    loadData() {
+      this.$http.get('https://europe-west1-focused-brand-202320.cloudfunctions.net/depatures/NSR:StopPlace:6332').then(response => {
+        this.departures = response.body;
+      }, error => {
+        console.log(error);
+      });
+    },
+    getDeparturesFromPlatform: (departures, platform) => {
+      return departures.filter((departure) => {
+        return departure.platform === platform;
+      })
+    },
+    getHumanTime: (time) => {
+      return Math.round((Date.parse(time) - Date.now())/(60*1000))
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>

b04ac1050fa8b095839275d67801575cfff514f3
Henrik Lilleengen <henrik@lilleengen.io>
2018-07-31 21:48:59 +0200

    Show human time

--- src/components/NRK.vue
+++ src/components/NRK.vue
@@ -2,13 +2,13 @@
     <div class="white-box">
         <div v-if="data[0]">
             <div>{{ data[0].title }}</div>
-            <div>{{ data[0].pubDate }}</div>
+            <div>{{ getHourMinute(data[0].pubDate) }}</div>
             <div>{{ data[0].content }}</div>
         </div>
         <div>
             <div v-for="news in data.slice(1, 4)">
                 <div>{{ news.title }}</div>
-                <div>{{ news.pubDate }}</div>
+                <div>{{ getHourMinute(news.pubDate) }}</div>
             </div>
         </div>
     </div>
@@ -37,7 +37,21 @@
         this.parser.parseURL('https://jsonp.afeld.me/?url=https%3A%2F%2Fwww.nrk.no%2Fnyheter%2Fsiste.rss', (err, feed) => {
           this.data = feed.items;
         });
-      }
+      },
+      getHourMinute(dateString) {
+        let date = new Date(dateString);
+        let hour = date.getHours();
+        if(hour < 10) {
+          hour = '0' + hour;
+        }
+
+        let minute = date.getMinutes();
+        if(minute < 10) {
+          minute = '0' + minute;
+        }
+
+        return hour + ':' + minute;
+      },
     }
   }
 </script>

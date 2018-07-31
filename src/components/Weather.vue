2ced5e44f33ea1e700eb52795a261ded21e7959d
Henrik Lilleengen <henrik@lilleengen.io>
2018-07-31 22:57:53 +0200

    Fix weather icons

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="weather">
         <div class="single-weather" v-for="(forcast, index) in weather" v-bind:key="index"> <!-- forcast.location.symbol.id -->
-            <img class="icon" src="../assets/weather/regn.svg" alt="regn" />
+            <img class="icon" :src="require('@/assets/weather/' + weatherToIconMapper[forcast.location.symbol.number])" />
             <span class="temp">
                 {{ parseInt(((parseInt(forcast.location.minTemperature.value) + parseInt(forcast.location.maxTemperature.value)) / 2) + 0.5) }}
             </span>
@@ -18,14 +18,57 @@
     data () {
       return {
         weather: [],
+        weatherToIconMapper: {
+          1: 'sol.svg',
+          2: 'sky-sol.svg',
+          3: 'sky-sol.svg',
+          15: 'sky.svg',
+          6: 'regn-lyn.svg',
+          24: 'regn-lyn.svg',
+          25: 'regn-lyn.svg',
+          26: 'regn-lyn.svg',
+          20: 'regn-lyn.svg',
+          27: 'regn-lyn.svg',
+          28: 'regn-lyn.svg',
+          21: 'regn-lyn.svg',
+          29: 'regn-lyn.svg',
+          30: 'regn-lyn.svg',
+          22: 'regn-lyn.svg',
+          11: 'regn-lyn.svg',
+          31: 'regn-lyn.svg',
+          23: 'regn-lyn.svg',
+          32: 'regn-lyn.svg',
+          33: 'regn-lyn.svg',
+          14: 'regn-lyn.svg',
+          34: 'regn-lyn.svg',
+          40: 'regn.svg',
+          5: 'regn.svg',
+          41: 'regn.svg',
+          42: 'regn.svg',
+          7: 'regn.svg',
+          43: 'regn.svg',
+          44: 'regn.svg',
+          8: 'regn.svg',
+          45: 'regn.svg',
+          46: 'regn.svg',
+          9: 'regn.svg',
+          10: 'regn.svg',
+          47: 'regn.svg',
+          12: 'regn.svg',
+          48: 'regn.svg',
+          49: 'regn.svg',
+          13: 'regn.svg',
+          50: 'regn.svg',
+        }
       }
     },
     mounted () {
       this.loadData();
 
       setInterval(function () {
+        console.log(this.weather[0].location.symbol.number);
         this.loadData();
-      }.bind(this), 15*60000);
+      }.bind(this), 30000);
     },
     methods: {
       loadData() {
@@ -56,6 +99,7 @@
     }
     .weather .icon{
         width: 84px;
+        height: 60px;
         padding-bottom: 10px;
     }
     .single-weather{
